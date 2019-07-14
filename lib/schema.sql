27a9ac0586fd1b567a25b12facc4c7f0edd926e0
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 14:26:38 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,23 @@
 CREATE TABLE Series (
   id INTEGER PRIMARY KEY 
-  title TEXT 
-  
+  title TEXT
+  author TEXT 
+  sub-genre TEXT
+);
+
+CREATE TABLE Sub-Genre (
+  id INTEGER PRIMARY KEY 
+  name TEXT
+);
+
+CREATE TABLE Authors (
+  id INTEGER PRIMARY KEY 
+  name TEXT
+);
+
+CREATE TABLE Books (
+  id INTEGER PRIMARY KEY 
+  title TEXT
+  year INTEGER 
+  series TEXT 
 );
