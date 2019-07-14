8ae3f70f6315766c6ef366507f4ca8ced159ed93
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 15:31:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,9 +25,8 @@ def select_name_and_series_subgenres_of_authors
   FROM series
   INNER JOIN author 
   ON series.author_id = authors.id
-  INNER JOIN subgenres 
-  ON "
-
+  INNER JOIN subgenres
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
