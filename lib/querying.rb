7b9ee524c3a2f197fbae9a55e4034343538269f7
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 15:38:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,8 @@ def select_series_title_with_most_human_characters
   INNER JOIN series
   ON characters.series_id = series.id
   GROUP BY series.title, species 
-  HAVING character.species = 'Human'
+  HAVING character.species = 'human'
+  ORDER BY 
   ."
 end
 
