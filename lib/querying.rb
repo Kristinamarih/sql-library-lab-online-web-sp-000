2548ee56a6fb8134fe4ec2d5bf8c7b41e8daa928
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-14 15:36:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title 
   FROM characters
   INNER JOIN series
-  ON characters.title_id = characters."
+  ON characters.series_id = series.id
+  ."
 end
 
 def select_character_names_and_number_of_books_they_are_in
